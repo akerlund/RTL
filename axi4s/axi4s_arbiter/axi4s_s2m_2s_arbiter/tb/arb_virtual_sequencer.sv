8304c28c8636f68845d870eef2b7ea263da1ca4f
akerlund <fredrik.akerlund.lh@outlook.com>
2020-06-06 14:57:36 +0200

    AXI4S|Arbiter: Slave to two masters

---
+++ axi4s/axi4s_arbiter/axi4s_s2m_2s_arbiter/tb/arb_virtual_sequencer.sv
@@ -0,0 +1,34 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2020 Fredrik Åkerlund
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Description:
+//
+////////////////////////////////////////////////////////////////////////////////
+
+class arb_virtual_sequencer extends uvm_virtual_sequencer;
+
+  `uvm_component_utils(arb_virtual_sequencer)
+
+  vip_axi4s_sequencer #(vip_axi4s_cfg) mst0_sequencer;
+  vip_axi4s_sequencer #(vip_axi4s_cfg) mst1_sequencer;
+  vip_axi4s_sequencer #(vip_axi4s_cfg) slv0_sequencer;
+
+  function new(string name = "virtual_sequencer", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+
+endclass
