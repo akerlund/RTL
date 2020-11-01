56a6993b692ba8eaa45525bfea7b552dd9c6db73
name <email>
2020-11-01 20:54:42 +0000

    update

--- scripts/pyreg/pyreg.py
+++ scripts/pyreg/pyreg.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 ##
-## Copyright (C) 2020 Fredrik ��kerlund
+## Copyright (C) 2020 Fredrik Åkerlund
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
