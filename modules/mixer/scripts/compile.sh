ca405f41b59cb5822b2ec3da3669ef231e25948a
akerlund <fredrik.akerlund.lh@outlook.com>
2021-03-29 21:10:59 +0200

    files.lst

--- modules/mixer/scripts/compile.sh
+++ modules/mixer/scripts/compile.sh
@@ -4,20 +4,32 @@ git_root="$(git rev-parse --show-toplevel)"
 
 # Specify the top files
 rtl_top=mixer
-uvm_top=mix_tb_top
+uvm_top="mix_tb_top"
 
-# Source other modules file lists
-source $git_root/vip/vip_fixed_point/uvm_files.lst
-source $git_root/vip/vip_math/uvm_files.lst
-source $git_root/vip/vip_axi4s_agent/uvm_files.lst
-source $git_root/modules/math/multiplication/rtl/rtl_files.lst
+# ------------------------------------------------------------------------------
+# Source submodules
+# ------------------------------------------------------------------------------
 
-# Source the module's file lists
-source ./rtl/rtl_files.lst
-source ./tb/uvm_files.lst
+git_sub_root="$(git rev-parse --show-toplevel)"
+
+git_root="$(git rev-parse --show-toplevel)/submodules/VIP"
+source $git_root/vip_fixed_point/files.lst
+source $git_root/vip_math/files.lst
+source $git_root/vip_axi4s_agent/files.lst
+
+
+# ------------------------------------------------------------------------------
+# Source modules
+# ------------------------------------------------------------------------------
+
+# Restoring the git root
+git_root="$(git rev-parse --show-toplevel)"
+source $git_root/modules/math/multiplication/rtl/files.lst
+source ./rtl/files.lst
+source ./tb/files.lst
 
 # Parameter override
-parameters+=("AUDIO_WIDTH_P=24 ")
-parameters+=("GAIN_WIDTH_P=24 ")
-parameters+=("NR_OF_CHANNELS_P=4 ")
-parameters+=("Q_BITS_P=7 ")
+parameters+="AUDIO_WIDTH_P=24 "
+parameters+="GAIN_WIDTH_P=24 "
+parameters+="NR_OF_CHANNELS_P=4 "
+parameters+="Q_BITS_P=7 "
