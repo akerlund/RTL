3d7e629d702a4ee7ba21c3b74c410b2797a4cd22
akerlund <fredrik.akerlund.lh@outlook.com>
2021-03-30 22:25:14 +0200

    Headers

--- modules/interfaces/axi4/rtl/axi4_if.sv
+++ modules/interfaces/axi4/rtl/axi4_if.sv
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 //
 // Copyright (C) 2021 Fredrik Åkerlund
-// https://github.com/akerlund
+// https://github.com/akerlund/RTL
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
