9eecb7db146d5baa30ad0a7f8b6cf1b48a9f7718
akerlund <fredrik.akerlund.lh@outlook.com>
2021-04-11 17:59:22 +0200

    FIFO development with RAM

--- modules/fifo/rtl/fifo.sv
+++ modules/fifo/rtl/fifo.sv
@@ -90,7 +90,7 @@ module fifo #(
     end
     else begin : generate_synchronous_ram_fifo
 
-      localparam int REG_ADDR_WIDTH_C = 1;
+      localparam int REG_ADDR_WIDTH_C = 2;
 
       // Generate with RAM
       logic   [ADDR_WIDTH_P-1 : 0] ram_write_address;
@@ -98,85 +98,15 @@ module fifo #(
       logic   [DATA_WIDTH_P-1 : 0] ram_read_data;
       logic   [ADDR_WIDTH_P   : 0] ram_fill_level;
 
-      logic                        reg_write_enable;
+      logic                        ram_read_enable;
+      logic                        ram_read_enable_d0;
+
       logic                        reg_full;
+      logic [REG_ADDR_WIDTH_C : 0] fifo_fill_level;
 
-      assign ram_fill_level  = (ram_write_address >= ram_read_address) ?
-                               {1'b0, ram_write_address} - {1'b0, ram_read_address} :
-                               FIFO_MAX_LEVEL_C - ({1'b0, ram_read_address} - {1'b0, ram_write_address});
-
-      assign ing_full        = sr_fill_level[ADDR_WIDTH_P] && !read_enable;
+      // Ports
+      assign ing_full        = sr_fill_level[ADDR_WIDTH_P];
       assign ing_almost_full = (sr_fill_level >= cr_almost_full_level);
-
-      // RAM read and write process
-      always_ff @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-          ram_write_address <='0;
-          ram_read_address  <='0;
-          reg_write_enable  <='0;
-        end
-        else begin
-
-          if (write_enable) begin
-            ram_write_address <= ram_write_address + 1;
-          end
-
-          // Reading from the RAM to the register FIFO
-          if (ram_fill_level > 0 && (!reg_full || read_enable)) begin
-            ram_read_address <= ram_read_address + 1;
-            reg_write_enable <= '1;
-          end else begin
-            reg_write_enable <= '0;
-          end
-        end
-      end
-
-      // Generate the FIFO's RAM
-      ram_sdp #(
-        .DATA_WIDTH_P        ( DATA_WIDTH_P      ),
-        .ADDR_WIDTH_P        ( ADDR_WIDTH_P      )
-      ) ram_sdp_i0 (
-
-        // Clock
-        .clk                 ( clk               ), // input
-
-        // Port A (write port)
-        .port_a_enable       ( '1                ), // input
-        .port_a_write_enable ( write_enable      ), // input
-        .port_a_data_ing     ( ing_data          ), // input
-        .port_a_address      ( ram_write_address ), // input
-
-        // Port B (read port)
-        .port_b_enable       ( '1                ), // input
-        .port_b_address      ( ram_read_address  ), // input
-        .port_b_data_egr     ( ram_read_data     )  // output
-      );
-
-      // Register at the output removes the delay of 1 clk period
-      // it takes for RAM memories to output data
-      fifo_register #(
-        .DATA_WIDTH_P    ( DATA_WIDTH_P     ),
-        .ADDR_WIDTH_P    ( REG_ADDR_WIDTH_C )
-      ) fifo_register_i0 (
-
-        // Clock and reset
-        .clk             ( clk              ), // input
-        .rst_n           ( rst_n            ), // input
-
-        // Ingress
-        .ing_enable      ( reg_write_enable ), // input
-        .ing_data        ( ram_read_data    ), // input
-        .ing_full        ( reg_full         ), // output
-
-        // Egress
-        .egr_enable      ( read_enable      ), // input
-        .egr_data        ( egr_data         ), // output
-        .egr_empty       ( egr_empty        ), // output
-
-        // Status registers
-        .sr_fill_level   (                  )  // output
-      );
-
 
       // Status process
       always_ff @(posedge clk or negedge rst_n) begin
@@ -199,6 +129,82 @@ module fifo #(
           end
         end
       end
+
+      // RAM read and write process
+      always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+          ram_fill_level     <= '0;
+          ram_read_enable    <= '0;
+          ram_read_enable_d0 <= '0;
+          ram_write_address  <= '0;
+          ram_read_address   <= '0;
+        end
+        else begin
+
+          if (write_enable) begin
+            ram_write_address <= ram_write_address + 1;
+          end
+
+          ram_fill_level <= (ram_write_address >= ram_read_address) ?
+                            {1'b0, ram_write_address} - {1'b0, ram_read_address} :
+                            FIFO_MAX_LEVEL_C - ({1'b0, ram_read_address} - {1'b0, ram_write_address});
+
+          ram_read_enable    <= ram_fill_level > 0 && (fifo_fill_level <= 2);
+          ram_read_enable_d0 <= ram_read_enable;
+
+          // Reading from the RAM to the register FIFO
+          if (ram_read_enable) begin
+            ram_read_address <= ram_read_address + 1;
+          end
+        end
+      end
+
+
+      // Generate the FIFO's RAM
+      ram_sdp #(
+        .DATA_WIDTH_P        ( DATA_WIDTH_P      ),
+        .ADDR_WIDTH_P        ( ADDR_WIDTH_P      )
+      ) ram_sdp_i0 (
+
+        // Clock
+        .clk                 ( clk               ), // input
+
+        // Port A (write port)
+        .port_a_enable       ( '1                ), // input
+        .port_a_write_enable ( write_enable      ), // input
+        .port_a_data_ing     ( ing_data          ), // input
+        .port_a_address      ( ram_write_address ), // input
+
+        // Port B (read port)
+        .port_b_enable       ( ram_read_enable   ), // input
+        .port_b_address      ( ram_read_address  ), // input
+        .port_b_data_egr     ( ram_read_data     )  // output
+      );
+
+      // Register at the output removes the delay of 1 clk period
+      // it takes for RAM memories to output data
+      fifo_register #(
+        .DATA_WIDTH_P    ( DATA_WIDTH_P       ),
+        .ADDR_WIDTH_P    ( REG_ADDR_WIDTH_C   )
+      ) fifo_register_i0 (
+
+        // Clock and reset
+        .clk             ( clk                ), // input
+        .rst_n           ( rst_n              ), // input
+
+        // Ingress
+        .ing_enable      ( ram_read_enable_d0 ), // input
+        .ing_data        ( ram_read_data      ), // input
+        .ing_full        ( reg_full           ), // output
+
+        // Egress
+        .egr_enable      ( read_enable        ), // input
+        .egr_data        ( egr_data           ), // output
+        .egr_empty       ( egr_empty          ), // output
+
+        // Status registers
+        .sr_fill_level   ( fifo_fill_level    )  // output
+      );
     end
   endgenerate
 
